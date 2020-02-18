e7d1daacbd57bb19b36e066645c27b617ddde38b
Yojhaira Gonzales <yojhaira.gonzales@gmail.com>
2020-02-17 20:23:23 -0500

    arreglar errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { AccessComponent } from './components/access/access.component';
