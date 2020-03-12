ec2181e23a6394ee5662c02f3833cd58f29c6cc3
Luis <luis_alexander_23_05_96@hotmail.com>
2020-03-11 21:56:55 -0500

    integrando servicio registrar usuario

--- src/app/template/register/register.template.ts
+++ src/app/template/register/register.template.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import {} from '../../service/usuario.service';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsuarioService} from '../../service/usuario.service';
+import { DOCUMENT } from '@angular/common';
+import { usuarioI } from 'src/app/models/usuario';
+import { Router} from '@angular/router'
 
 @Component({
     selector: 'register-template',
     templateUrl: './register.template.html',
-    styleUrls: ['./register.template.scss']
+    styleUrls: ['./register.template.scss'],
+    providers: [UsuarioService ]
 })
 
 export class RegisterTemplate implements OnInit {
     // Properties
-    // public formRegister: any;
+    public formRegister: any;
+    public showError:boolean;
 
     constructor(
         // private formBuilder: FormBuilder,
