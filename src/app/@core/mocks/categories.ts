de8bb9e71defa26b24dbc00ecb8691aec4644920
Kevin Rodriguez <kevin.rodriguez@tivane.pe>
2020-09-27 19:59:33 -0500

    Actualizar iconos de categorias
    Se agrego iconos para categorias actualizando main-content.template.html y categories.template.html. Se dividio en 2 archivos dentro de assets: icons.svg para iconos del sistema y categories.svg en icons/sprite para iconos de categorias. Se movio la lista de categorias de job-categorias.service.ts a un mock en @core
    Changes to be committed:
    	modified:   src/app/services/job-categories/job-categories.service.ts
    	modified:   src/app/template/categories/categories.template.html
    	modified:   src/app/template/main/main-content/main-content.template.html
    	modified:   src/assets/images/icons.svg
    
    	new file:   src/app/@core/mocks/categories.ts
    	new file:   src/app/@core/models/categories.model.ts
    	new file:   src/assets/images/icons/sprite/categories.svg

---
+++ src/app/@core/mocks/categories.ts
@@ -0,0 +1,101 @@
+import { Categories } from '../models/categories.model';
+
+export const CATEGORIESLIST: Categories[] = [
+    {
+        categoryID: 1,
+        categoryName: "Carpintería",
+        categoryIconName: "carpinteria",
+        categoryImageName: "carpinteria"
+    },
+    {
+        categoryID: 2,
+        categoryName: "Mecánica",
+        categoryIconName: "mecanica",
+        categoryImageName: "mecanica"
+    },
+    {
+        categoryID: 3,
+        categoryName: "Jardinería",
+        categoryIconName: "jardineria",
+        categoryImageName: "jardineria"
+    },
+    {
+        categoryID: 4,
+        categoryName: "Panadería",
+        categoryIconName: "panaderia",
+        categoryImageName: "panaderia"
+    },
+    {
+        categoryID: 5,
+        categoryName: "Informática",
+        categoryIconName: "informatica",
+        categoryImageName: "informatica"
+    },
+    {
+        categoryID: 6,
+        categoryName: "Pintores",
+        categoryIconName: "pintores",
+        categoryImageName: "pintores"
+    },
+
+    {
+        categoryID: 7,
+        categoryName: "Electricistas",
+        categoryIconName: "electricistas",
+        categoryImageName: "electricistas"
+    },
+    {
+        categoryID: 8,
+        categoryName: "Meserxs",
+        categoryIconName: "meserxs",
+        categoryImageName: "meserxs"
+    },
+    {
+        categoryID: 9,
+        categoryName: "Docentes",
+        categoryIconName: "docentes",
+        categoryImageName: "docentes"
+    },
+    {
+        categoryID: 10,
+        categoryName: "Construcción",
+        categoryIconName: "construccion",
+        categoryImageName: "construccion"
+    },
+    {
+        categoryID: 11,
+        categoryName: "Conductores",
+        categoryIconName: "conductores",
+        categoryImageName: "conductores"
+    },
+    {
+        categoryID: 12,
+        categoryName: "Artistas",
+        categoryIconName: "artistas",
+        categoryImageName: "artistas"
+    },
+    {
+        categoryID: 13,
+        categoryName: "Gasfitería",
+        categoryIconName: "gasfiteria",
+        categoryImageName: "gasfitería"
+    },
+    {
+        categoryID: 14,
+        categoryName: "Wedding Planner",
+        categoryIconName: "wedding-planner",
+        categoryImageName: "wedding-planner"
+    },
+    {
+        categoryID: 15,
+        categoryName: "Fiestas",
+        categoryIconName: "fiestas",
+        categoryImageName: "fiestas"
+    },
+    {
+        categoryID: 16,
+        categoryName: "Mozos",
+        categoryIconName: "mozos",
+        categoryImageName: "mozos"
+    }
+]
--- src/app/services/job-categories/job-categories.service.ts
+++ src/app/services/job-categories/job-categories.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { CATEGORIESLIST } from 'src/app/@core/mocks/categories';
 
 @Injectable({
     providedIn: 'root'
 })
+
 export class JobService {
 
     public collectionCategories: Array<any>;
@@ -17,45 +19,13 @@ export class JobService {
     }
 
     public listCategoriesWorkers() {
-        return this.collectionCategories = [
-            {
-                categoryID: 1,
-                categoryName: "Jardinería",
-                categoryIconName: "jardineria",
-                categoryImageName: "jardineria"
-            },
-            {
-                categoryID: 2,
-                categoryName: "Pintores",
-                categoryIconName: "pintores",
-                categoryImageName: "pintores"
-            },
-            {
-                categoryID: 3,
-                categoryName: "Electricistas",
-                categoryIconName: "electricistas",
-                categoryImageName: "electricistas"
-            },
-            {
-                categoryID: 4,
-                categoryName: "Carpintería",
-                categoryIconName: "carpinteria",
-                categoryImageName: "carpinteria"
-            },
-            {
-                categoryID: 5,
-                categoryName: "Construcción",
-                categoryIconName: "construccion",
-                categoryImageName: "construccion"
-            }
-        ]
+        return this.collectionCategories = CATEGORIESLIST;
         // return this._http.get('http://chamba-back.test/api/oficio');
     }
 
     public cleanCharacters(input: string): string {
-
-        let acentos = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç";
-        let original = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc";
+        let acentos = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç ";
+        let original = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc-";
         for (var i = 0; i < acentos.length; i++) {
             input = input.replace(acentos.charAt(i), original.charAt(i)).toLowerCase();
         }
