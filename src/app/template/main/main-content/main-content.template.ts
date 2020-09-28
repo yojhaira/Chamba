dfebe7b2eec9e2ea2b3ffe41aba95c0ea38eb2c4
Kevin Rodriguez <kevin.rodriguez@tivane.pe>
2020-09-28 00:30:46 -0500

    Errores menores
    Fueron corregidos errores presentados en el template main-content y la inicializacion del objeto para el funcionamiento del carrusel, en login se paso el argumento que requeria la funcion para iniciar sesion y en toolbar se activo manualmente una de las opciones en este caso el componente que se muestra cuando no ha iniciado sesion

--- src/app/template/main/main-content/main-content.template.ts
+++ src/app/template/main/main-content/main-content.template.ts
@@ -36,34 +36,75 @@ export class MainContentTemplate implements OnInit, OnDestroy {
         public _ctaLoginService: CTALoginService
     ) { }
 
-    ngOnInit() {
+    public ngOnInit(): void {
         //this.listCategories();
-        this.orientationCTA = "left";
         this.showCategoriesMain();
         this.showDistritosMain();
+
+        this.buttonOwlPrev = `<div class='button-content'>
+            <div class='md-icon'>
+                <i class="fas fa-chevron-left"></i>
+            </div>
+        </div>`;
+
+        this.buttonOwlNext = `<div class='button-content'>
+            <div class='md-icon'>
+                <i class="fas fa-chevron-right"></i>
+            </div>
+        </div>`;
+
+        this.customOptions = {
+            loop: true,
+            mouseDrag: true,
+            touchDrag: true,
+            pullDrag: false,
+            dots: false,
+            navSpeed: 700,
+            navText: [this.buttonOwlPrev, this.buttonOwlNext],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                400: {
+                    items: 1
+                },
+                580: {
+                    items: 2
+                },
+                800: {
+                    items: 3
+                },
+                1080: {
+                    items: 4
+                }
+            },
+            nav: true
+        }
+
+        this.orientationCTA = "left";
     }
 
     public ngOnDestroy(): void {
-        if(this.categoriesSubcription) {
+        if (this.categoriesSubcription) {
             this.categoriesSubcription.unsubscribe();
         }
     }
-    
+
     public showCategoriesMain() {
         this.categoriesSubcription = this._jobService.listCategoriesWorkers().subscribe(res => {
             console.table(res.response);
             const dataResponse = JSON.parse(JSON.stringify(res));
             this.arrayCategoriesMain = dataResponse.response;
         });
-        
-    }
-    
 
-    public showDistritosMain(){
+    }
+
+
+    public showDistritosMain() {
         this._distritoService.listar().subscribe(
             result => {
                 console.log(result);
-                const dataResponse  = JSON.parse(JSON.stringify(result))
+                const dataResponse = JSON.parse(JSON.stringify(result))
                 this.distritos = dataResponse.response;
             },
             error => {
