2414e34420fbfd20a54eb05862a33d2a436a0ada
Yojhaira <yojhaira.gonzales@gmail.com>
2020-09-29 04:20:02 -0500

    Actualizar Header Page(component)
    Se actualizo helper.service para cambiar los espacios por guiones y se actulizo categories.template.html para setear nombre de imagen de categoria

--- src/app/@core/services/helper.service.ts
+++ src/app/@core/services/helper.service.ts
@@ -10,8 +10,8 @@ export class HelperService {
 
     public cleanCharacters(input: string): string {
 
-        let acentos = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç";
-        let original = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc";
+        let acentos = "ÃÀÁÄÂÈÉËÊÌÍÏÎÒÓÖÔÙÚÜÛãàáäâèéëêìíïîòóöôùúüûÑñÇç ";
+        let original = "AAAAAEEEEIIIIOOOOUUUUaaaaaeeeeiiiioooouuuunncc-";
         for (var i = 0; i < acentos.length; i++) {
             input = input.replace(acentos.charAt(i), original.charAt(i)).toLowerCase();
         }
